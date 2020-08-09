0edb2b14a6e5a29aa8e5760b9d517a5125a80b94
Wahaj <wahajmaqsood20@gmail.com>
2020-08-09 17:39:39 +0500

    Added background.

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,78 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+
+interface props {
+  lang: string;
+}
+
+function LanguageButton(prop: props) {
+  let path = "../" + prop.lang.toLowerCase() + ".png";
+  return (
+    <button className="language-button">
+      <img className="language-icon" src={path} alt={path}></img>
+    </button>
+  );
+}
+
+function ButtonBar() {
+  return (
+    <div className="button-bar">
+      <LanguageButton lang="C++" />
+      <LanguageButton lang="Python" />
+      <LanguageButton lang="JavaScript" />
+      <LanguageButton lang="CSharp" />
+    </div>
+  );
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <ButtonBar />
+      <button className="vscode-button"></button>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
     </div>
   );
 }
